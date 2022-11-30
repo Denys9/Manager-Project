b147fac8a342ff2b76db30d48e8a615f4b13c56d
Dines <lozindenis6@gmail.com>
2022-11-30 17:27:45 +0200

    Create "Manager Project"

--- main.py
+++ main.py
@@ -5,15 +5,13 @@ product = input('Введіть назву продукту - ')
 category = input('Введіть назву категорії - ')
 amount = input('Введіть кількість - ')
 csv_columns = ['Продукт', 'Категорія', 'Кількість']
-csv_columns1 = ['login', 'password']
+
 dict_data = [
     {'Продукт': product, 'Категорія': category, 'Кількість': amount}
 ]
-dict_data1 = [
-    {'login': login, 'password': password}
-]
+
 csv_file = "manager.csv"
-csv_file = "user data.csv"
+
 try:
     with open(csv_file, 'w') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
@@ -22,11 +20,16 @@ try:
             writer.writerow(data)
 except IOError:
     print("I/O error")
+    csv_columns = ['login', 'password']
+    dict_data = [
+        {'login': login, 'password': password}
+    ]
+    csv_file = "user data.csv"
     try:
         with open(csv_file, 'w') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=csv_columns1)
+            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
             writer.writeheader()
-            for data in dict_data1:
+            for data in dict_data:
                 writer.writerow(data)
     except IOError:
         print("I/O error")
