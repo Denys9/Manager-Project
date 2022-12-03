d8752e524f1791ee8840730d79cdc47c087604a4
Denys9 <113623082+Denys9@users.noreply.github.com>
2022-12-03 10:37:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,58 @@
-
-
 import csv
-
-product = input('Введіть назву продукту - ')
-category = input('Введіть назву категорії - ')
-amount = input('Введіть кількість - ')
-description = input('Введіть опис - ')
-price = input('Введіть ціну товара - ')
-store = input('Введіть магазин купівлі - ')
-csv_columns = ['Продукт', 'Категорія', 'Кількість', 'Опис', 'Ціна', 'Магазин']
-
-dict_data = [
-    {'Продукт': product, 'Категорія': category, 'Кількість': amount,
-     'Опис': description, 'Ціна': price, 'Магазин': store}
-]
-
-csv_file = "manager.csv"
-
 try:
-    with open(csv_file, 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
-        writer.writeheader()
-        for data in dict_data:
-            writer.writerow(data)
-except IOError:
-    print("I/O error")
+    while True:
+        begin = input('Ви маєте акаунт?(y/n)')
+        datafile = 'data.txt'
+        if begin == 'n':
+            print('Створення акаунту')
+            loginc = input('Придумайте логін - ')
+            passwordc = input('Придумайте пароль - ')
+            csv_columns = [loginc, passwordc]
+            try:
+                with open(datafile, 'w') as csvfile:
+                    writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+                    writer.writeheader()
+            except IOError:
+                print('I/P error')
 
+            continue
+        if begin == 'y':
+            print('Вхід в акаунт')
+            loginw = input('Логін - ')
+            passwordw = input('Пароль - ')
+            with open(datafile, 'r') as csvfile:
+                write = csvfile.readline().split(',')
+            print(bool(loginw==write[0]))
+            product = input('Введіть назву продукту - ')
+            category = input('Введіть назву категорії - ')
+            amount = input('Введіть кількість - ')
+            description = input('Введіть опис - ')
+            price = input('Введіть ціну товара - ')
+            store = input('Введіть магазин купівлі - ')
+            csv_columns1 = ['Продукт', 'Категорія', 'Кількість', 'Опис', 'Ціна', 'Магазин']
+
+            dict_data = [
+                {'Продукт': product, 'Категорія': category, 'Кількість': amount,
+                 'Опис': description, 'Ціна': price, 'Магазин': store}
+            ]
+
+            csv_file = "manager.csv"
+
+            try:
+                with open(csv_file, 'w') as csvfile:
+                    writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=csv_columns1)
+                    writer.writeheader()
+                    for data in dict_data:
+                        writer.writerow(data)
+            except IOError:
+                print("I/O error")
+            if loginw != write[0]:
+                print('Невірний логін')
+            if passwordw != write[1]:
+                print('Невірний пароль')
+            else:
+                break
+        else:
+            print('Перевірте ввод символів!')
+except Exception as e:
+    print(e)
